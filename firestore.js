a8ef9e640962b69bd36fa73b2fa78937c80e3135
Luis Angel <19170051@uttcampus.edu.mx>
2022-12-29 01:29:39 -0600

    diseño nuevo 1

--- firestore.js
+++ firestore.js
@@ -26,3 +26,6 @@ export const saveuser = (nombre,adelanto,fecha_asignada)=>
 addDoc(collection(db, "users"),{nombre,adelanto,fecha_asignada});
 export const getusers= () =>getDocs(collection(db, "users"));
 export const updateusers = (id,field) => updateDoc(doc(db, "users",id),field);
+export const updateconfiguraciones = (precio)=>
+updateDoc(collection(db, "configuraciones","Vq4uaOxlUotSqCRdwnbe"),{precio});
+export const getconfiguraciones= () =>getDocs(collection(db, "configuraciones"));
