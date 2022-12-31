6b5d1de08b42c39a1f9ca521e9232a7ab1b7b1ce
Luis Angel <19170051@uttcampus.edu.mx>
2022-12-31 01:36:08 -0600

    arreglar array de modals

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-import { saveuser,getusers } from "../firestore.js";
+import { saveuser,getusers,getconfiguraciones } from "../firestore.js";
 
 window.addEventListener("DOMContentLoaded", () => {
   
 });
 let fecha_asignada = null;
  var array = [];
+let costo = "";
  async function reload_dates(){
   const querysnapshot = await getusers();
   querysnapshot.forEach(doc => {
@@ -12,9 +13,33 @@ let fecha_asignada = null;
      array.push(fecha);
  })
  }
+ const querysnapshot = await getconfiguraciones();
+ querysnapshot.forEach((doc) => {
+   costo = doc.data().precio.precio;
+   document.getElementById("recordar_costo").innerHTML =
+  `
+  <p class="">  <span style="font-weight:800;">Nuevo usuario  /  Costo de la renta :${costo}</span></p>
+  `;
+
+   let adelanto_input = document.getElementById("adelanto");
+   adelanto_input.setAttribute("min",1);
+   adelanto_input.setAttribute("max",costo);
+   adelanto_input.addEventListener('input',function(){
+   
+    if (this.value.length > costo.length)
+    { 
+       this.value = this.value.slice(0,4); 
+    }
+    if (parseInt(this.value) > costo){
+    alert("no se puede superar el costo se regresara al estado incial intentelo de nuevo");
+    adelanto_input.value ="";
+    }
+  })
+   $("#Configuraciones").modal("hide");
+ }); 
+
  reload_dates();
  
-
 $("#doi").datepicker({
   closeText: 'Cerrar',
   prevText: '<Ant',
@@ -46,7 +71,10 @@ form.addEventListener("submit", (e) => {
   const info = fecha_asignada.split('/');
   fecha_asignada = info[2] + '-' + info[0] + '-' + info[1];
   const name = document.getElementById("nombre").value;
-  const adelanto = document.getElementById("adelanto").value;
+  let adelanto = document.getElementById("adelanto").value;
+  if (parseInt(adelanto) ==costo){
+    adelanto ="pagado";
+  }
   saveuser(name, adelanto,fecha_asignada);
   reload_dates();
   form.reset()
